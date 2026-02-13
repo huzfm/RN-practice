83fa524803bad91664283219bf297428bcdeb0b0
huzfm <huzaaifmushtaq@gmail.com>
2026-02-13 10:59:17 +0530

    feat: Add a new Home component with modal functionality and basic UI elements.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import img from "@/assets/images/react-logo.png";
+import { useState } from "react";
 import {
   Image,
   Text,
@@ -7,9 +8,11 @@ import {
   Button,
   Alert,
   Pressable,
+  Modal,
 } from "react-native";
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <>
       <View
@@ -20,13 +23,34 @@ export default function Home() {
         }}
       >
         <Button
-          title="CLICK"
-          onPress={() => Alert.alert("Hello from expo")}
+          title="Open"
+          onPress={() => setIsVisible(true)}
           color="midnightblue"
         />
         <Pressable onLongPress={() => Alert.alert("Text pressed")}>
           <Text>Hello</Text>
         </Pressable>
+        <Modal
+          visible={isVisible}
+          onRequestClose={() => setIsVisible(false)}
+          animationType="slide"
+          presentationStyle="pageSheet" // only works on ios
+        >
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "red",
+              padding: 60,
+            }}
+          >
+            <Text>Hello from modal</Text>
+            <Button
+              title="Close"
+              color="green"
+              onPress={() => setIsVisible(false)}
+            />
+          </View>
+        </Modal>
       </View>
     </>
   );
