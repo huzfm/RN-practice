eea438040489043cdde5504e2437e66889c94991
huzfm <huzaaifmushtaq@gmail.com>
2026-02-15 15:20:10 +0530

    feat: Add stylesheet component demonstrating basic React Native styling with red and blue boxes.

--- components/stylesheet.tsx
+++ components/stylesheet.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function Style() {
   return (
-    <View>
-      <Text>stylesheet</Text>
+    <View style={styles.container}>
+      <View style={[styles.box, styles.redBox]}>
+        <Text>Red</Text>
+      </View>
+      <View style={[styles.box, styles.blueBox]}>
+        <Text>Blue</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 60,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    paddingStart: 10,
+    margin: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 10,
+  },
+  redBox: {
+    backgroundColor: "red",
+  },
+  blueBox: {
+    backgroundColor: "blue",
+  },
+});
