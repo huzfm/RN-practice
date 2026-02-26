32ac8182addf3c0ca212d6c743106353cf03ce52
huzfm <huzaaifmushtaq@gmail.com>
2026-02-26 15:57:32 +0530

    feat: Add root layout for Expo Router with stack navigation for index and about screens.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{
-      headerStyle: { backgroundColor: "red" },
+      headerStyle: { backgroundColor: "black" },
     }}>
       <Stack.Screen
         name="index"
