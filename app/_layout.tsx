8babdb374921d92e440b7977a6d042e85d556f1c
huzfm <huzfm@proto.me>
2026-02-26 15:51:49 +0530

    feat: Add root layout with Stack navigator for index and about screens.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,21 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{
+      headerStyle: { backgroundColor: "red" },
+    }}>
       <Stack.Screen
         name="index"
         options={{
           title: "Home",
-          headerStyle: { backgroundColor: "lightblue" },
+          headerTintColor: "black",
+          headerTitleStyle: { fontWeight: "semibold" },
+        }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{
+          title: "About",
           headerTintColor: "black",
           headerTitleStyle: { fontWeight: "semibold" },
         }}
